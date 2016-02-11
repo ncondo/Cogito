24d073fb64f082825fcea8364f368a2a36f1f25c
Nick <nicholas.condo@gmail.com>
2016-02-11 00:01:01 -0500

    Implement Bishop

---
+++ Cogito/src/com/chess/engine/pieces/Bishop.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.chess.engine.pieces;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import com.chess.engine.Color;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+
+/**
+ * @author ncondo
+ *
+ */
+public class Bishop extends Piece {
+	
+	private final static int[] POSSIBLE_MOVE_OFFSETS = { -9, -7, 7, 9 };
+
+	Bishop(int piecePosition, Color pieceColor) {
+		super(piecePosition, pieceColor);
+	}
+	
+	@Override
+	public Collection<Move> calculateLegalMoves(final Board board) {
+		
+		int possibleDestination;
+		final List<Move> legalMoves = new ArrayList<>();
+		
+		for (final int currentOffset : POSSIBLE_MOVE_OFFSETS) {
+			possibleDestination = this.piecePosition;
+			
+			while (BoardUtils.isValidTileCoordinate(possibleDestination)) {
+				
+				if (isFirstColumnExclusion(possibleDestination, currentOffset) ||
+						isEighthColumnExclusion(possibleDestination, currentOffset)) {
+					break;
+				}
+				possibleDestination += currentOffset;
+				
+				if (BoardUtils.isValidTileCoordinate(possibleDestination)) {
+					final Tile possibleDestinationTile = board.getTile(possibleDestination);
+					
+					if (!possibleDestinationTile.isTileOccupied()) {
+						legalMoves.add(new Move.MajorMove(board, this, possibleDestination));
+					} else {
+						final Piece pieceAtDestination = possibleDestinationTile.getPiece();
+						final Color pieceColor = pieceAtDestination.getPieceColor();
+						
+						if (this.pieceColor != pieceColor) {
+							legalMoves.add(new Move.AttackMove(board, this, possibleDestination, 
+									pieceAtDestination));
+						}
+						break;
+					}
+				}
+			}
+		}
+		return Collections.unmodifiableList(legalMoves);
+	}
+	
+	private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+		return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || 
+				candidateOffset == 7);
+	}
+	
+	private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
+		return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || 
+				candidateOffset == 9);
+	}
+
+}
--- Cogito/src/com/chess/engine/pieces/Knight.java
+++ Cogito/src/com/chess/engine/pieces/Knight.java
@@ -62,25 +62,21 @@ public class Knight extends Piece {
 	}
 	
 	private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
-		
 		return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -17 ||
 				candidateOffset == -10 || candidateOffset == 6 || candidateOffset == 15);
 	}
 	
 	private static boolean isSecondColumnExclusion(final int currentPosition, final int candidateOffset) {
-		
 		return BoardUtils.SECOND_COLUMN[currentPosition] && (candidateOffset == -10 ||
 				candidateOffset == 6);
 	}
 	
 	private static boolean isSeventhColumnExclusion(final int currentPosition, final int candidateOffset) {
-		
 		return BoardUtils.SEVENTH_COLUMN[currentPosition] && (candidateOffset == -6 ||
 				candidateOffset == 10);
 	}
 	
 	private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
-		
 		return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -15 ||
 				candidateOffset == -6 || candidateOffset == 10 || candidateOffset == 17);
 	}
