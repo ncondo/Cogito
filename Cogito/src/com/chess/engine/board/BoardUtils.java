6d2afd703ef622766cf5f03e9dceab0b26891db0
Nick <nicholas.condo@gmail.com>
2016-04-19 23:41:06 -0400

    Introduce algebraic notation

--- Cogito/src/com/chess/engine/board/BoardUtils.java
+++ Cogito/src/com/chess/engine/board/BoardUtils.java
@@ -3,6 +3,8 @@
  */
 package com.chess.engine.board;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -22,11 +24,12 @@ public final class BoardUtils {
 	public static final boolean[] SEVENTH_ROW = initRow(8);
 	public static final boolean[] EIGHTH_ROW = initRow(0);
 	
+	public static final int START_TILE_INDEX = 0;
 	public static final int NUM_TILES = 64;
 	public static final int NUM_TILES_PER_ROW = 8;
 	
-	//public static final String[] ALGEBREIC_NOTATION = initializeAlgebreicNotation();
-	//public static final Map<String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
+	public static final String[] ALGEBRAIC_NOTATION = initializeAlgebraicNotation();
+	public static final Map<String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
 	
 	private BoardUtils() {
 		throw new RuntimeException("Cannot instantiate BoardUtils");
@@ -52,17 +55,38 @@ public final class BoardUtils {
 		return column;
 	}
 	
+	private static String[] initializeAlgebraicNotation() {
+		return new String[] {
+				"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+				"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+				"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+				"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+				"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+				"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+				"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+				"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+		};
+	}
+	
+	private static Map<String, Integer> initializePositionToCoordinateMap() {
+		final Map<String, Integer> positionToCoordinate = new HashMap<>();
+		for (int i = START_TILE_INDEX; i < NUM_TILES; i++) {
+			positionToCoordinate.put(ALGEBRAIC_NOTATION[i], i);
+		}
+		return Collections.unmodifiableMap(positionToCoordinate);
+	}
+	
 	public static boolean isValidTileCoordinate(final int coordinate) {
 		return coordinate >=0 && coordinate < NUM_TILES;
 	}
-	/*
+	
 	public static int getCoordinateAtPosition(final String position) {
-		POSITION_TO_COORDINATE.get(position);
+		return POSITION_TO_COORDINATE.get(position);
 	}
 	
 	public static String getPositionAtCoordinate(final int coordinate) {
-		return ALGEBREIC_NOTATION[coordinate];
+		return ALGEBRAIC_NOTATION[coordinate];
 	}
-	*/
+	
 
 }
