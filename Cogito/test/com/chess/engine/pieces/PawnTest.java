1e4ab81e4d59aedb213d6e757318d10d92d5fbe9
Nick <nicholas.condo@gmail.com>
2016-05-03 00:27:27 -0400

    Add JUnit to build path and create test packages

---
+++ Cogito/test/com/chess/engine/pieces/PawnTest.java
@@ -0,0 +1,14 @@
+package com.chess.engine.pieces;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PawnTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
